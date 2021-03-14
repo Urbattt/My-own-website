a349cb42694186e37f2f668b34e29c2ebf510920
Urbattt <80606638+Urbattt@users.noreply.github.com>
2021-03-13 17:12:20 -0800

    Add files via upload

---
+++ Coding/main.js
@@ -0,0 +1,28 @@
+function changecolor(){
+    document.getElementById("headtag").style.color="red";
+    
+    
+    
+    
+}
+function switchpic() {
+document.getElementById("image1").src= "WIN_20200809_15_22_57_Pro.jpg";  
+    
+    
+    
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
